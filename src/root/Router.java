39a9595bf7c1085c62eb8065f335077c6bb66233
Patrick Desmarais <patrick.desm@gmail.com>
2015-02-17 20:42:16 -0500

    Added simple methods to simulate a battle in the arena.

--- src/root/Router.java
+++ src/root/Router.java
@@ -1,6 +1,8 @@
 package root;
 
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 
@@ -12,5 +14,19 @@ public class Router {
 	@Produces("text/plain")
 	public String get() {
 		return "Welcome to the Arena!";
+	}
+
+	@Path(value="/players")
+	@GET
+	@Produces("text/plain")
+	public String getPlayers() {
+		return "Sandor, Arya and Claude";
+	}
+
+	@POST
+	@Consumes("text/plain")
+	@Produces("text/plain")
+	public String battle(String playerName) {
+		return "You won!"; // "You lose :(";
 	}
 }
